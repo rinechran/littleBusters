85dabb417dcfcb587fb962a007ca87b03fba381a
Eun Sik Jung <rinechran@outlook.jp>
2018-06-02 11:49:09 +0900

    Debug Helper

--- littleBusters/KUD/DebugHelper/Debug.h
+++ littleBusters/KUD/DebugHelper/Debug.h
@@ -1,17 +1,57 @@
 #pragma once
 
 #include <iostream>
+#include <Windows.h>
+#include <functional>
+#include <DbgHelp.h>
+
+#pragma comment(lib,"dbghelp.lib")
+
 
 
 namespace KUD {
-	template <class T>
-	void DEBUGMSG(T &&msg) {
+	namespace DEBUG {
+
+		template <class T>
+		void MSG(T &&msg) {
 #ifdef _DEBUG
-		std::cout << msg << std::endl;
+			std::cout << msg << std::endl;
 #endif // DEBUG
+		}
+
+		class ImpleUnhanderFiter {
+		public:
+			ImpleUnhanderFiter() :fn(nullptr){}
+
+			void operator()() {
+				fn();
+			}
+
+			template <typename T>
+			bool operator==(T &&oth) {
+				return fn == oth;
+			}
+			template <typename T>
+			bool operator!=(T &&oth) {
+				return fn != oth;
+			}
+
+		private:
+			std::function<void()> fn;
+
+		} ImpleUnhanderFiters;
+
+		LONG __stdcall unhanderExceptFiter(PEXCEPTION_POINTERS exceptionPointer) {
+			if (ImpleUnhanderFiters != nullptr) {
+				ImpleUnhanderFiters();
+			}
+			return 0;
+		}
+		void SetUnhanderExcept() {
+			SetUnhandledExceptionFilter(unhanderExceptFiter);
+		}
+
+
+
 	}
-	/*
-		char * + __LINE__ operator not implement
-		
-	*/
 }
